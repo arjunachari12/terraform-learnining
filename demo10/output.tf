167b4709351fdd4f728f2936c33cc1740f9a2bcf
arjunachari12 <arjun.achari12@gmail.com>
2021-08-25 16:47:15 +0530

    azure, gcr files

---
+++ demo10/output.tf
@@ -0,0 +1,3 @@
+output "myecr-url" {
+  value = aws_ecr_repository.myecr.repository_url
+}
---
+++ demo12/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("lunar-bonus-313308-4c70d37637dd.json")
+
+  project = "lunar-bonus-313308"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
