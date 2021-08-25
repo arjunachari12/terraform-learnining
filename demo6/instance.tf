5f261c3dee050e4a654c4a1eb5fabc72b9c9853a
arjunachari12 <arjun.achari12@gmail.com>
2021-08-25 11:06:49 +0530

    added complete ec2, vpc provisioning scripts

---
+++ demo6/instance.tf
@@ -0,0 +1,26 @@
+data "aws_ami" "example" {
+  executable_users = ["self"]
+  most_recent      = true
+  name_regex       = "^myami-\\d{3}"
+  owners           = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["myami-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.example.id
+  instance_type = "t1.micro"
+}
